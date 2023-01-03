777ce14ef788b0fa5f8e028f2efe05d2e046bd89
Natalia Burduk <nataliaburduk@gmail.com>
2023-01-03 09:38:43 +0100

    add carusel ability

--- js/components/cakes_carousel.js
+++ js/components/cakes_carousel.js
@@ -36,16 +36,50 @@ class CakeCarousel {
         image: "img/cakes/cake_6.jpg"
       }
     ];
+    this.visibleAmount = 4;
+    this.firstVisibleIndex = 0;
+    this.nextBtn = document.querySelector('.next'),
+    this.prevBtn = document.querySelector('.previous');
   }
 
   render () {
     const wrapper = document.querySelector(this.parentSelector);
-    this.cakes.forEach((cake, i) => {
-      const cakeCard = new CakeCard(i, cake.name, cake.description, cake.image);
+    while (wrapper.firstChild) {
+      wrapper.removeChild(wrapper.lastChild);
+    }
+    this.cakesToShow().forEach((cake, i) => {
+      const cakeCard = new CakeCard(i, cake.name, cake.description, cake.image, i < this.visibleAmount);
       wrapper.append(cakeCard.htmlElement());
     });
   }
 
-}
+  showNextSlide () {
+    this.nextBtn.addEventListener('click', () => {
+      if (this.firstVisibleIndex > this.visibleAmount - 1) {
+        this.firstVisibleIndex = 0;
+      } else {
+        this.firstVisibleIndex++;
+      }
+      this.render();
+    });
+  }
 
+  showPrevSlide(){
+    this.prevBtn.addEventListener('click', () => {
+      if (this.firstVisibleIndex == 0) {
+        this.firstVisibleIndex = this.cakes.length - 1;
+      } else {
+        this.firstVisibleIndex--;
+      }
+      this.render();
+    });
+  }
+
+  cakesToShow () {
+    const arr1 = this.cakes.slice(this.firstVisibleIndex, this.firstVisibleIndex + this.visibleAmount);
+    const howMany = this.visibleAmount - arr1.length;
+    const arr2 = this.cakes.slice(0, howMany);
+    return arr1.concat(arr2);
+  }
+}
 export default CakeCarousel;
--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ import CakeCarousel from "./components/cakes_carousel";
 
 const cakeCarousel = new CakeCarousel('#wrapper');
 cakeCarousel.render();
+cakeCarousel.showNextSlide();
+cakeCarousel.showPrevSlide();
+
