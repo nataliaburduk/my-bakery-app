0080982a3e139362e3b1325cd0aa3e2f712ee921
Natalia Burduk <nataliaburduk@gmail.com>
2023-01-04 14:52:43 +0100

    style slider-buttons

--- js/components/cakes_slider.js
+++ js/components/cakes_slider.js
@@ -40,6 +40,16 @@ class CakeSlider {
         name: "Chimney Cakes",
         description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
         image: "img/cakes/cake_6.jpg"
+      },
+      {
+        name: "Healthy sweets",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "img/cakes/cake_7.jpg"
+      },
+      {
+        name: "Tarts",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "img/cakes/cake_8.jpg"
       }
     ];
 
