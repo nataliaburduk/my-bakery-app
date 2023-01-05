14cae82d5413c33cdc72616809a58998a7fe73d3
Natalia Burduk <nataliaburduk@gmail.com>
2023-01-05 12:37:48 +0100

    get data from api

--- js/components/cakes_slider.js
+++ js/components/cakes_slider.js
@@ -6,53 +6,11 @@ import {
 } from "../screen_breakpoints";
 
 import CakeCard from "./cake_card";
-
+ 
 class CakeSlider {
   constructor(parentSelector, screenWidth, ...classes) {
     this.parentSelector = parentSelector;
-    this.classes = classes;
-    this.cakes = [{
-        name: "Cakes",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_1.jpg"
-      },
-      {
-        name: "Cookies",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_2.jpg"
-      },
-      {
-        name: "Chocolate",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_3.jpg"
-      },
-      {
-        name: "Macaroons",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_4.jpg"
-      },
-      {
-        name: "Panettone",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_5.jpg"
-      },
-      {
-        name: "Chimney Cakes",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_6.jpg"
-      },
-      {
-        name: "Healthy sweets",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_7.jpg"
-      },
-      {
-        name: "Tarts",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        image: "img/cakes/cake_8.jpg"
-      }
-    ];
-
+    this.classes = classes;   
     this.screenWidth = screenWidth;
     this.firstVisibleIndex = 0;
     this.lastVisibleIndex = this.calcLastVisibleIndex();
@@ -62,56 +20,61 @@ class CakeSlider {
   }
 
   render() {
-    const cardsToShow = this.cakes.slice(this.firstVisibleIndex, this.lastVisibleIndex + 1);
+    fetch('http://localhost:3000/cakes')
+      .then(data =>  data.json())
+      .then(res => this.renderFetchedCards(res))
+      .catch(_error => this.hideArrows());   
+  }
+
+  renderFetchedCards(cakes) {
+    const cardsToShow = cakes.slice(this.firstVisibleIndex, this.lastVisibleIndex + 1);
     cardsToShow.forEach((cake, i) => {
       const cakeCard = new CakeCard(i, cake.name, cake.description, cake.image, true);
       this.wrapper.append(cakeCard.htmlElement());
     });
-
-    this.showNextSlide();
-    this.showPrevSlide();  
+    this.showNextSlide(cakes);
+    this.showPrevSlide(cakes); 
   }
 
-  showNextSlide() {
+  showNextSlide(cakes) {
     this.nextBtn.addEventListener('click', () => {
-      this.firstVisibleIndex = this.incrementedIndex(this.firstVisibleIndex);
-      this.lastVisibleIndex = this.incrementedIndex(this.lastVisibleIndex);
+      this.firstVisibleIndex = this.incrementedIndex(this.firstVisibleIndex, cakes.length);
+      this.lastVisibleIndex = this.incrementedIndex(this.lastVisibleIndex, cakes.length);
 
-      const cake = this.cakes[this.lastVisibleIndex];
+      const cake = cakes[this.lastVisibleIndex];
       const cakeCard = new CakeCard(this.lastVisibleIndex, cake.name, cake.description, cake.image, true);
       this.wrapper.removeChild(this.wrapper.querySelector('div'));
       this.wrapper.append(cakeCard.htmlElement());
     });
   }
 
-  showPrevSlide() {
+  showPrevSlide(cakes) {
     this.prevBtn.addEventListener('click', () => {
-      this.firstVisibleIndex = this.decrementedIndex(this.firstVisibleIndex);
-      this.lastVisibleIndex = this.decrementedIndex(this.lastVisibleIndex);
+      this.firstVisibleIndex = this.decrementedIndex(this.firstVisibleIndex, cakes.length);
+      this.lastVisibleIndex = this.decrementedIndex(this.lastVisibleIndex, cakes.length);
 
-      const cake = this.cakes[this.firstVisibleIndex];
+      const cake = cakes[this.firstVisibleIndex];
       const cakeCard = new CakeCard(this.firstVisibleIndex, cake.name, cake.description, cake.image, true);
       this.wrapper.insertBefore(cakeCard.htmlElement(), this.wrapper.querySelector('div'));
       this.wrapper.removeChild(this.wrapper.lastChild);
     });
   }
 
-  incrementedIndex(index) {
-    if (index === this.cakes.length - 1) {
+  incrementedIndex(index, cakeCount) {
+    if (index === cakeCount - 1) {
       return 0;
     } else {
       return ++index;
     }
   }
 
-  decrementedIndex(index) {
+  decrementedIndex(index, cakeCount) {
     if (index === 0) {
-      return this.cakes.length - 1;
+      return cakeCount - 1;
     } else {
       return --index;
     }
   }
-
 
   calcLastVisibleIndex() {
     if (this.screenWidth < SMALL_SCREEN_WIDTH) {
@@ -126,7 +89,10 @@ class CakeSlider {
       return 5;
     }
   }
+
+  hideArrows() {
+    this.nextBtn.hidden = 'true';
+    this.prevBtn.hidden = 'true';
+  }
 }
-
-
 export default CakeSlider;
