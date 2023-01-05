6f87e0a2c4938eb152a92774bf09f047f7df5ef3
Natalia Burduk <nataliaburduk@gmail.com>
2023-01-05 17:07:37 +0100

    improve welcome screen

--- js/components/welcome-screen.js
+++ js/components/welcome-screen.js
@@ -1,10 +1,12 @@
 function hideWelcomeScreen() {
   const welcomeBtn = document.querySelector('.welcome-btn'),
-        welcomeContainer = document.querySelector('.welcome-container img');
+        welcomeContainer = document.querySelector('.welcome-container'),
+        scrollBody = document.querySelector('body');
   
   welcomeBtn.addEventListener('click', () => {
     welcomeContainer.classList.add('hide-welcome');
-    welcomeBtn.classList.add('hide-welcome');
+    scrollBody.setAttribute('scroll', 'yes');
+    scrollBody.style.overflow = 'visible';    
   });
 }
 
